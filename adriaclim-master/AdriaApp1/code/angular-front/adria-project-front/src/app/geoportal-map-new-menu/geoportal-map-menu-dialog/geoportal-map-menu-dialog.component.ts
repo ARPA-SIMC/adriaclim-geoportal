93522069cf4f2d1fc5671e183d542390ada675f2
Simone Guidi <79375278+SimoneGuidi92@users.noreply.github.com>
2023-11-08 17:25:51 +0100

    + multiselect menu modale

--- adriaclim-master/AdriaApp1/code/angular-front/adria-project-front/src/app/geoportal-map-new-menu/geoportal-map-menu-dialog/geoportal-map-menu-dialog.component.ts
+++ adriaclim-master/AdriaApp1/code/angular-front/adria-project-front/src/app/geoportal-map-new-menu/geoportal-map-menu-dialog/geoportal-map-menu-dialog.component.ts
@@ -4,6 +4,7 @@ import { Component, ElementRef, Inject, ViewChild, ChangeDetectorRef, AfterConte
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSelect } from '@angular/material/select';
+import * as _ from 'lodash';
 import { HttpService } from 'src/app/services/http.service';
 
 @Component({
@@ -15,6 +16,7 @@ import { HttpService } from 'src/app/services/http.service';
 export class GeoportalMapMenuDialogComponent implements OnInit, AfterViewInit {
 
   @ViewChild('quartoMatSelect') quartoMatSelect!: MatSelect;
+  @ViewChild("searchInput") searchInput: ElementRef | undefined;
 
   activeLayersArray: any;
   form!: FormGroup;
@@ -181,12 +183,27 @@ export class GeoportalMapMenuDialogComponent implements OnInit, AfterViewInit {
 
   }
 
+  setFilter() {
+    console.log("SET FILTER");
+    // this.searchInput!.nativeElement.value = '';
+    if(this.searchInput) {
+      if(this.searchInput.nativeElement.value && this.searchInput.nativeElement.value !== "") {
+        console.log("SEARCH INPUT =", this.searchInput.nativeElement.value);
+
+        this.onKey(this.searchInput.nativeElement.value);
+
+      }
+    }
+  }
+
   /**
    * Funzione che cambia dinamicamente gli elementi all'interno dei vari menu
    */
   changeSel(type?: string) {
+    console.log("TYPE =", type);
     console.log("FORM =", this.form.get("category")?.value);
-
+    // this.menuDatasets = [];
+    // this.searchFilter = [];
 
     let tmpScale: any[] = [];
     let tmpTimeperiods: any[] = [];
@@ -197,9 +214,9 @@ export class GeoportalMapMenuDialogComponent implements OnInit, AfterViewInit {
       this.resAllNodes.forEach((el: any) => {
         // console.log("FORM CATEGORY =", this.form.get("category")?.value[0]);
 
-        this.form.get("category")?.value.every((cat: any) => {
+        this.form.get("category")?.value.forEach((cat: any) => {
           if(cat === el.adriaclim_dataset) {
-            console.log("CONTROLLO OK");
+            // console.log("CONTROLLO OK");
             tmpScale.push(el.adriaclim_scale);
 
           }
@@ -207,58 +224,54 @@ export class GeoportalMapMenuDialogComponent implements OnInit, AfterViewInit {
         });
 
       });
+      // this.scaleDatasets = [];
+      // this.timeperiodDatasets = [];
       this.scaleDatasets = [...new Set(tmpScale)];
-      console.log("SCALE DATASETS =", this.scaleDatasets);
-
+      // console.log("SCALE DATASETS =", this.scaleDatasets);
       this.form.get("scale")?.setValue([this.scaleDatasets[0]]);
+      // console.log("FORM CATEGORY =", this.form.get("category")?.value);
+      // console.log("FORM SCALE =", this.form.get("scale")?.value);
 
       this.resAllNodes.forEach((el: any) => {
-        this.form.get("category")?.value.every((cat: any) => {
-          this.form.get("scale")?.value.every((scale: any) => {
-            if(cat === el.adriaclim_dataset && scale === el.adriaclim_scale) {
-              tmpTimeperiods.push(el.adriaclim_timeperiod);
-            }
+        this.form.get("category")?.value.forEach((cat: any) => {
+          // console.log("CAT =", cat);
+          // console.log("SCALE =", this.form.get("scale")?.value[0]);
 
-          });
+          if(cat === el.adriaclim_dataset && this.form.get("scale")?.value[0] === el.adriaclim_scale) {
+            tmpTimeperiods.push(el.adriaclim_timeperiod);
+
+          }
 
         });
 
       });
+      // this.timeperiodDatasets = [];
+
       this.timeperiodDatasets = [...new Set(tmpTimeperiods)];
+      // console.log("TIMEPERIOD DATASETS =", this.timeperiodDatasets);
+
       this.form.get("timeperiod")?.setValue([this.timeperiodDatasets[0]]);
-      let categoryMatch: any;
-      let scaleMatch: any;
-      let timeperiodMatch: any;
+      let condizione1: any;
+      let condizione2: any;
+      let condizione3: any;
 
       this.menuDatasets = this.resAllNodes.filter((el: any) => {
-        // el.adriaclim_dataset === this.form.get("category")?.value && el.adriaclim_scale === this.form.get("scale")?.value && el.adriaclim_timeperiod === this.form.get("timeperiod")?.value
-        // categoryMatch = this.form.get("category")?.value.every((category: any) => category === el.adriaclim_dataset);
-        // scaleMatch = this.form.get("scale")?.value.every((scale: any) => scale === el.adriaclim_scale);
-        // timeperiodMatch = this.form.get("timeperiod")?.value.every((timeperiod: any) => timeperiod === el.adriaclim_timeperiod);
-        // return categoryMatch && scaleMatch && timeperiodMatch;
-        this.form.get("category")?.value.forEach((cat: any) => {
-          this.form.get("scale")?.value.forEach((scale: any) => {
-            this.form.get("timeperiod")?.value.forEach((timeperiod: any) => {
-              if(cat === el.adriaclim_dataset && scale === el.adriaclim_scale && timeperiod === el.adriaclim_timeperiod) {
-                categoryMatch = true;
-                scaleMatch = true;
-                timeperiodMatch = true;
-              }
-            });
-          });
-        });
-        return categoryMatch && scaleMatch && timeperiodMatch;
+        // condizione1 = this.form.get("category")?.value.every((category: any) => category === el.adriaclim_dataset);
+        condizione1 = this.form.get("category")?.value.includes(el.adriaclim_dataset);
+        condizione2 = this.form.get("scale")?.value[0] === el.adriaclim_scale;
+        condizione3 = this.form.get("timeperiod")?.value[0] === el.adriaclim_timeperiod;
+        return condizione1 && condizione2 && condizione3;
       });
-      console.log("CATEGORY MATCH =", categoryMatch);
-      console.log("SCALE MATCH =", scaleMatch);
-      console.log("TIMEPERIOD MATCH =", timeperiodMatch);
+      // console.log("CATEGORY MATCH =", condizione1);
+      // console.log("SCALE MATCH =", condizione2);
+      // console.log("TIMEPERIOD MATCH =", condizione3);
 
 
     }
     else if(type === "s") {
       this.resAllNodes.forEach((el: any) => {
-        this.form.get("category")?.value.every((cat: any) => {
-          this.form.get("scale")?.value.every((scale: any) => {
+        this.form.get("category")?.value.forEach((cat: any) => {
+          this.form.get("scale")?.value.forEach((scale: any) => {
             if(cat === el.adriaclim_dataset && scale === el.adriaclim_scale) {
               tmpTimeperiods.push(el.adriaclim_timeperiod);
 
@@ -269,37 +282,38 @@ export class GeoportalMapMenuDialogComponent implements OnInit, AfterViewInit {
         })
       });
       this.timeperiodDatasets = [...new Set(tmpTimeperiods)];
+      // console.log("TIMEPERIOD SCALE DATASETS =", this.timeperiodDatasets);
+
       this.form.get("timeperiod")?.setValue([this.timeperiodDatasets[0]]);
+
+      let condizione1: any;
+      let condizione2: any;
+      let condizione3: any;
+
+      this.menuDatasets = this.resAllNodes.filter((el: any) => {
+        condizione1 = this.form.get("category")?.value.includes(el.adriaclim_dataset);
+        condizione2 = this.form.get("scale")?.value.includes(el.adriaclim_scale);
+        condizione3 = this.form.get("timeperiod")?.value.includes(el.adriaclim_timeperiod);
+        return condizione1 && condizione2 && condizione3;
+      });
 
     }
     else if(type === "t") {
-      let categoryMatch: any;
-      let scaleMatch: any;
-      let timeperiodMatch: any;
+      let condizione1: any;
+      let condizione2: any;
+      let condizione3: any;
       this.menuDatasets = this.resAllNodes.filter((el: any) => {
-        // el.adriaclim_dataset === this.form.get("category")?.value && el.adriaclim_scale === this.form.get("scale")?.value && el.adriaclim_timeperiod === this.form.get("timeperiod")?.value
-        // const categoryMatch = this.form.get("category")?.value.every((category: any) => category === el.adriaclim_dataset);
-        // const scaleMatch = this.form.get("scale")?.value.every((scale: any) => scale === el.adriaclim_scale);
-        // const timeperiodMatch = this.form.get("timeperiod")?.value.every((timeperiod: any) => timeperiod === el.adriaclim_timeperiod);
-        // return categoryMatch && scaleMatch && timeperiodMatch;
-        this.form.get("category")?.value.forEach((cat: any) => {
-          this.form.get("scale")?.value.forEach((scale: any) => {
-            this.form.get("timeperiod")?.value.forEach((timeperiod: any) => {
-              if(cat === el.adriaclim_dataset && scale === el.adriaclim_scale && timeperiod === el.adriaclim_timeperiod) {
-                categoryMatch = true;
-                scaleMatch = true;
-                timeperiodMatch = true;
-              }
-
-            });
-          });
-        });
-        return categoryMatch && scaleMatch && timeperiodMatch;
+        condizione1 = this.form.get("category")?.value.includes(el.adriaclim_dataset);
+        condizione2 = this.form.get("scale")?.value.includes(el.adriaclim_scale);
+        condizione3 = this.form.get("timeperiod")?.value.includes(el.adriaclim_timeperiod);
+        return condizione1 && condizione2 && condizione3;
       });
 
     }
 
     this.searchFilter = this.menuDatasets;
+    // console.log("SEARCH FILTER =", this.searchFilter);
+
   }
 
   /**
@@ -307,8 +321,16 @@ export class GeoportalMapMenuDialogComponent implements OnInit, AfterViewInit {
    */
   onKey(event: any) {
     // console.log("EVENT =", event.target.value);
+    if(event) {
+      this.searchFilter = this.search(event.target.value);
 
-    this.searchFilter = this.search(event.target.value);
+    }
+    else if(typeof event === "string" && event !== "") {
+      this.searchFilter = this.search(event);
+    }
+    else {
+      this.searchFilter = this.search(null);
+    }
     // console.log("SEARCH FILTER DOPO FILTRO =", this.searchFilter);
 
 
@@ -317,11 +339,11 @@ export class GeoportalMapMenuDialogComponent implements OnInit, AfterViewInit {
   /**
    * Funzione che permette di effettuare una ricerca tra i vari dataset
    */
-  search(value: string) {
+  search(value: any) {
     if(value) {
       let filters = value.toLowerCase().split(" ");
-
-      let menuFiltered = this.menuDatasets.filter((option: any) => {
+      let menuDatasetClone = _.cloneDeep(this.menuDatasets);
+      let menuFiltered = menuDatasetClone.filter((option: any) => {
         let optionTitle = option.title.toLowerCase();
         let optionInstitution = option.institution.toLowerCase();
         return filters.every((filter: any) => {
@@ -338,11 +360,13 @@ export class GeoportalMapMenuDialogComponent implements OnInit, AfterViewInit {
 
         });
       });
-
+      console.log("IF VALUE MENU FILTERED =", menuFiltered);
       return menuFiltered;
 
     }
     else {
+      console.log("ELSE MENU DATASET =", this.menuDatasets);
+
       return this.menuDatasets;
     }
   }
