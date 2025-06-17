829bc0c1df6e2bc018802cb88167a11f78c3bfd2
alexandra-dulas <alexandra.dulas@fos.it>
2025-06-17 10:06:37 +0200

    Update external_wms.py

--- adriaclim-master/code/AdriaProject/Dataset/external_wms.py
+++ adriaclim-master/code/AdriaProject/Dataset/external_wms.py
@@ -24,66 +24,78 @@ def fetch_wms_response(url):
         return JsonResponse({'error': str(e)}, status=500)
 
 def layers2DNew(request):
-    """Handles 2D layer requests."""
-    params = {
-        'service': request.GET.get('service'),
-        'request': request.GET.get('request'),
-        'layers': request.GET.get('layers'),
-        'styles': request.GET.get('styles'),
-        'format': request.GET.get('format'),
-        'transparent': request.GET.get('transparent'),
-        'version': request.GET.get('version'),
-        'width': request.GET.get('width'),
-        'height': request.GET.get('height'),
-        'crs': request.GET.get('crs'),
-        'bbox': request.GET.get('bbox'),
-        'time': request.GET.get('time'),
-        'bgcolor': request.GET.get('bgcolor'),
-    }
-    dataset_id = params['layers'].partition(":")[0]
-    url = build_wms_url(ERDDAP_URL, dataset_id, params)
-    return fetch_wms_response(url)
+    service=request.GET['service']
+    request1=request.GET['request']
+    layers=request.GET['layers']
+    styles=request.GET['styles']
+    format=request.GET['format']
+    transparent=request.GET['transparent']
+    version=request.GET['version']
+    width=request.GET['width']
+    height=request.GET['height']
+    crs=request.GET['crs']
+    bbox=request.GET['bbox']
+    time=request.GET['time']
+    bgcolor=request.GET['bgcolor']
+    dataset_id=layers.partition(":")[0]
+    url=ERDDAP_URL+"/wms/"+dataset_id+"/request?&service="+service+"&request="+request1+"&layers="+layers+"&styles="+styles+"&format="+format+"&transparent="+transparent+"&version="+version+"&bgcolor="+bgcolor+"&time="+time+"&width="+width+"&height="+height+"&crs="+crs+"&bbox="+bbox
+    requests_response = requests.get(url)
+    django_response = HttpResponse(
+            content=requests_response.content,
+            status=requests_response.status_code,
+            content_type=requests_response.headers['Content-Type']
+        )
+    return django_response
 
-def layers3DNew(request, parameter):
-    """Handles 3D layer requests."""
-    params = {
-        'service': request.GET.get('service'),
-        'request': request.GET.get('request'),
-        'layers': request.GET.get('layers'),
-        'styles': request.GET.get('styles'),
-        'format': request.GET.get('format'),
-        'transparent': request.GET.get('transparent'),
-        'version': request.GET.get('version'),
-        'width': request.GET.get('width'),
-        'height': request.GET.get('height'),
-        'crs': request.GET.get('crs'),
-        'bbox': request.GET.get('bbox'),
-        'time': request.GET.get('time'),
-        'bgcolor': request.GET.get('bgcolor'),
-        parameter: request.GET.get(parameter),
-    }
-    dataset_id = params['layers'].partition(":")[0]
-    url = build_wms_url(ERDDAP_URL, dataset_id, params)
-    return fetch_wms_response(url)
+def layers3DNew(request,parameter):
+    service=request.GET['service']
+    request1=request.GET['request']
+    layers=request.GET['layers']
+    styles=request.GET['styles']
+    format=request.GET['format']
+    transparent=request.GET['transparent']
+    version=request.GET['version']
+    width=request.GET['width']
+    height=request.GET['height']
+    crs=request.GET['crs']
+    bbox=request.GET['bbox']
+    time=request.GET['time']
+    bgcolor=request.GET['bgcolor']
+    value_param=request.GET[parameter]
+    dataset_id=layers.partition(":")[0]
+    url=ERDDAP_URL+"/wms/"+dataset_id+"/request?&service="+service+"&request="+request1+"&layers="+layers+"&styles="+styles+"&format="+format+"&transparent="+transparent+"&version="+version+"&bgcolor="+bgcolor+"&time="+time+"&"+parameter+"="+value_param+"&width="+width+"&height="+height+"&crs="+crs+"&bbox="+bbox
+    requests_response = requests.get(url)
+    django_response = HttpResponse(
+            content=requests_response.content,
+            status=requests_response.status_code,
+            content_type=requests_response.headers['Content-Type']
+        )
+        
+    return django_response
 
-def overlaysNew(request, dataset_id):
-    """Handles overlay requests."""
-    params = {
-        'service': request.GET.get('service'),
-        'request': request.GET.get('request'),
-        'layers': request.GET.get('layers'),
-        'styles': request.GET.get('styles'),
-        'format': request.GET.get('format'),
-        'transparent': request.GET.get('transparent'),
-        'version': request.GET.get('version'),
-        'width': request.GET.get('width'),
-        'height': request.GET.get('height'),
-        'crs': request.GET.get('crs'),
-        'bbox': request.GET.get('bbox'),
-        'bgcolor': request.GET.get('bgcolor'),
-    }
-    url = build_wms_url(ERDDAP_URL, dataset_id, params)
-    return fetch_wms_response(url)
+def overlaysNew(request,dataset_id):
+    service=request.GET['service']
+    request1=request.GET['request']
+    layers=request.GET['layers']
+    styles=request.GET['styles']
+    format=request.GET['format']
+    transparent=request.GET['transparent']
+    version=request.GET['version']
+    width=request.GET['width']
+    height=request.GET['height']
+    crs=request.GET['crs']
+    bbox=request.GET['bbox']
+    bgcolor=request.GET['bgcolor']
+    url=ERDDAP_URL+"/wms/"+dataset_id+"/request?&service="+service+"&request="+request1+"&layers="+layers+"&styles="+styles+"&format="+format+"&transparent="+transparent+"&version="+version+"&bgcolor="+bgcolor+"&width="+width+"&height="+height+"&crs="+crs+"&bbox="+bbox
+    requests_response = requests.get(url)
+
+    django_response = HttpResponse(
+            content=requests_response.content,
+            status=requests_response.status_code,
+            content_type=requests_response.headers['Content-Type']
+        )
+        
+    return django_response
 
 
 
