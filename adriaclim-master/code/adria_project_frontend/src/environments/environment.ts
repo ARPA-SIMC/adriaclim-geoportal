c18895ab805bb4f8227b606f9329a59a45d30303
alexandra-dulas <alexandra.dulas@fos.it>
2025-11-11 15:08:03 +0100

    Update environment.prod.ts

--- adriaclim-master/code/adria_project_frontend/src/environments/environment.ts
+++ adriaclim-master/code/adria_project_frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  backendUrl: "http://localhost:8000/",
+  backendUrl: "http://172.19.99.37:8000/", // for test
+  // backendUrl: "http://172.19.99.34:8000/", // for prod
   erddapUrl: "https://erddap-adriaclim.cmcc-opa.eu/erddap"
 };
