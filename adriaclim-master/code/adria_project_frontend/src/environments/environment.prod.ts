75e35d7700ce35080950089fab8749e77c799906
alexandra-dulas <alexandra.dulas@fos.it>
2026-05-28 14:48:55 +0200

    Update backend URL to adriaclim.eu

--- adriaclim-master/code/adria_project_frontend/src/environments/environment.prod.ts
+++ adriaclim-master/code/adria_project_frontend/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  backendUrl: "http://172.19.99.37:8000/", // for test branch
-  // backendUrl: "https://geoportale-adriaclim.datamb.it/" // for prod branch
+  backendUrl: "https://adriaclim.eu/",
   erddapUrl: "https://erddap-adriaclim.cmcc-opa.eu/erddap"
 };
